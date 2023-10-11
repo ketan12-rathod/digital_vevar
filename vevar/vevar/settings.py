8c473ee6f970153ae7252601b5af4a298266ba71
ketan12-rathod <rathodketan5112001@gmail.com>
2023-10-11 18:34:38 +0530

    five commit

--- vevar/vevar/settings.py
+++ vevar/vevar/settings.py
@@ -92,6 +92,9 @@ DATABASES = {
     }
 }
 
+
+database_url = os.environ.get('DATABASE_URL')
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
